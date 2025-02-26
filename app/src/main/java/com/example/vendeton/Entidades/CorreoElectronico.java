5f367f1f35f79739b64db135b8af747d0b118803
farid <faridardila22@gmail.com>
2025-02-26 15:18:13 -0500

    activity editar cliente ya tiene layout completo, procedimiento para traer correos ya es funcional

---
+++ app/src/main/java/com/example/vendeton/Entidades/CorreoElectronico.java
@@ -0,0 +1,17 @@
+package com.example.vendeton.Entidades;
+
+public class CorreoElectronico {
+
+    public String cor_usuario, cor_dominio, cor_correo;
+    public int cor_id, con_identificacion;
+
+    public CorreoElectronico(String cor_usuario, String cor_dominio, String cor_correo, int cor_id, int con_identificacion) {
+        this.cor_usuario = cor_usuario;
+        this.cor_dominio = cor_dominio;
+        this.cor_correo = cor_correo;
+        this.cor_id = cor_id;
+        this.con_identificacion = con_identificacion;
+    }
+
+
+}
