ef7ee30e9687550b4ce1010a274d05a686735ec1
farid <faridardila22@gmail.com>
2025-02-26 00:06:09 -0500

    pagina de editar info de cliente

---
+++ app/src/main/java/com/example/vendeton/Activitys/activity_pagina_inicial.java
@@ -0,0 +1,32 @@
+package com.example.vendeton.Activitys;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.example.vendeton.R;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+
+public class activity_pagina_inicial extends AppCompatActivity {
+
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagina_inicio);
+    }
+
+}
---
+++ app/src/main/java/com/example/vendeton/Entidades/BalanceGeneral.java
@@ -0,0 +1,11 @@
+package com.example.vendeton.Entidades;
+
+public class BalanceGeneral {
+    public BalanceGeneral(String tip_nombre, float monto) {
+        this.tip_nombre = tip_nombre;
+        this.monto = monto;
+    }
+
+    public String tip_nombre;
+    public float monto;
+}
