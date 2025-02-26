dcafabe208c621a7575c8fe5e24588d427c6dc9a
farid <faridardila22@gmail.com>
2025-02-25 22:19:33 -0500

    mini arreglo

--- app/src/main/java/com/example/vendeton/Activitys/activity_iniciar_sesion.java
+++ app/src/main/java/com/example/vendeton/Activitys/activity_iniciar_sesion.java
@@ -11,7 +11,7 @@ public class activity_iniciar_sesion extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_iniciar_sesion);
+        setContentView(R.layout.activity_inicio_de_sesion);
 
     }
 }
