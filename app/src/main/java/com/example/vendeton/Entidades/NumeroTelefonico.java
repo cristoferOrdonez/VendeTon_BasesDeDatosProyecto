c42bc7e162c6996ecbf37ecaa787c354d59fbb1e
farid <faridardila22@gmail.com>
2025-03-01 16:07:50 -0500

    insercion de correos en la base

--- app/src/main/java/com/example/vendeton/Entidades/NumeroTelefonico.java
+++ app/src/main/java/com/example/vendeton/Entidades/NumeroTelefonico.java
@@ -7,7 +7,7 @@ import androidx.annotation.NonNull;
 
 public class NumeroTelefonico implements Parcelable {
 
-    public NumeroTelefonico(int num_id, int con_identificacion, int num_prefijo, long num_numero, long num_numero_de_contacto) {
+    public NumeroTelefonico(int num_id, long con_identificacion, int num_prefijo, long num_numero, long num_numero_de_contacto) {
         this.num_id = num_id;
         this.con_identificacion = con_identificacion;
         this.num_prefijo = num_prefijo;
@@ -15,14 +15,14 @@ public class NumeroTelefonico implements Parcelable {
         this.num_numero_de_contacto = num_numero_de_contacto;
     }
 
-    public int num_id, con_identificacion, num_prefijo;
-    public long num_numero, num_numero_de_contacto;
+    public int num_id, num_prefijo;
+    public long num_numero, num_numero_de_contacto, con_identificacion;
 
 
     // Parcelable Constructor
     protected NumeroTelefonico(Parcel in) {
         num_id = in.readInt();
-        con_identificacion = in.readInt();
+        con_identificacion = in.readLong();
         num_prefijo = in.readInt();
         num_numero = in.readLong();
         num_numero_de_contacto = in.readLong();
@@ -51,7 +51,7 @@ public class NumeroTelefonico implements Parcelable {
     @Override
     public void writeToParcel(@NonNull Parcel dest, int flags) {
         dest.writeInt(num_id);
-        dest.writeInt(con_identificacion);
+        dest.writeLong(con_identificacion);
         dest.writeInt(num_prefijo);
         dest.writeLong(num_numero);
         dest.writeLong(num_numero_de_contacto);
