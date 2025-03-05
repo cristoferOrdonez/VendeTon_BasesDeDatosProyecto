b93876ddb97cb4a260374c11100cbfd90911cc70
cristoferOrdonez <crordonezo@unal.edu.co>
2025-03-04 22:31:53 -0500

    algunas correcciones

--- app/src/main/java/com/example/vendeton/Activitys/ActivitySeleccionUsuario.java
+++ app/src/main/java/com/example/vendeton/Activitys/ActivitySeleccionUsuario.java
@@ -1,5 +1,7 @@
 package com.example.vendeton.Activitys;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -20,49 +22,74 @@ public class ActivitySeleccionUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccion_usuario);
-        AccederAdmin = findViewById(R.id.accederAdministrador);
-        AccederClienteMayorista = findViewById(R.id.accederClienteMayorista);
-        AccederUsuarioPublico = findViewById(R.id.accederUusarioPublico);
 
-        if(VendeTon.estadoUsuario == VendeTon.ADMINISTRADOR){
-            Toast.makeText(this,"ADMINISTRADOR",Toast.LENGTH_SHORT);
-            Intent intent = new Intent(this, activity_pagina_inicial.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //builder.setTitle("Eliminar Evento");
+        //builder.setMessage("¿Está seguro de que desea eliminar este evento?");
 
-        } else if(VendeTon.estadoUsuario == VendeTon.CLIENTE_MAYORISTA){
-            Toast.makeText(this,"CLIENTE_MAYORISTA",Toast.LENGTH_SHORT);
-            Intent intent = new Intent(this, activity_info_cliente.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-        }
-        else if(VendeTon.estadoUsuario == VendeTon.USUARIO_PUBLICO) {
-            Toast.makeText(this,"USUARIO_PUBLICO",Toast.LENGTH_SHORT);
-            Intent intent = new Intent(this, activity_informe_catalogo.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-        }
-        else{
-            Toast.makeText(this,"SIN_REGISTRAR",Toast.LENGTH_SHORT);
-        }
+        builder.setPositiveButton("Eliminar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
 
 
-        AccederAdmin.setOnClickListener((View view) -> {
-            Intent intent = new Intent(this, activity_iniciar_sesion_admin.class);
-            startActivity(intent);
+            }
         });
 
-        AccederClienteMayorista.setOnClickListener((View view) -> {
-            Intent intent = new Intent(this, activity_iniciar_sesion_cliente.class);
-            startActivity(intent);
+        builder.setNegativeButton("Editar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                AccederAdmin = findViewById(R.id.accederAdministrador);
+                AccederClienteMayorista = findViewById(R.id.accederClienteMayorista);
+                AccederUsuarioPublico = findViewById(R.id.accederUusarioPublico);
+
+                if(VendeTon.estadoUsuario == VendeTon.ADMINISTRADOR){
+                    Toast.makeText(ActivitySeleccionUsuario.this,"ADMINISTRADOR",Toast.LENGTH_SHORT);
+                    Intent intent = new Intent(ActivitySeleccionUsuario.this, activity_pagina_inicial.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+
+                } else if(VendeTon.estadoUsuario == VendeTon.CLIENTE_MAYORISTA){
+                    Toast.makeText(ActivitySeleccionUsuario.this,"CLIENTE_MAYORISTA",Toast.LENGTH_SHORT);
+                    Intent intent = new Intent(ActivitySeleccionUsuario.this, activity_info_cliente.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+                else if(VendeTon.estadoUsuario == VendeTon.USUARIO_PUBLICO) {
+                    Toast.makeText(ActivitySeleccionUsuario.this,"USUARIO_PUBLICO",Toast.LENGTH_SHORT);
+                    Intent intent = new Intent(ActivitySeleccionUsuario.this, activity_informe_catalogo.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(ActivitySeleccionUsuario.this,"SIN_REGISTRAR",Toast.LENGTH_SHORT);
+                }
+
+
+                AccederAdmin.setOnClickListener((View view) -> {
+                    Intent intent = new Intent(ActivitySeleccionUsuario.this, activity_iniciar_sesion_admin.class);
+                    startActivity(intent);
+                });
+
+                AccederClienteMayorista.setOnClickListener((View view) -> {
+                    Intent intent = new Intent(ActivitySeleccionUsuario.this, activity_iniciar_sesion_cliente.class);
+                    startActivity(intent);
+                });
+
+                AccederUsuarioPublico.setOnClickListener((View view) -> {
+                    VendeTon.username = "usuario_publico";
+                    VendeTon.password = "";
+                    Intent intent = new Intent(ActivitySeleccionUsuario.this, activity_informe_catalogo.class);
+                    startActivity(intent);
+                });
+
+            }
         });
 
-        AccederUsuarioPublico.setOnClickListener((View view) -> {
-            VendeTon.username = "usuario_publico";
-            VendeTon.password = "";
-            Intent intent = new Intent(this, activity_informe_catalogo.class);
-            startActivity(intent);
-        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+
 
     }
 }
